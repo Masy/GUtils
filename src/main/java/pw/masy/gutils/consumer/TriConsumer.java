a9496c6c308dccc3a639d0ef332db16c657d7fa3
Masy <dev@masy.pw>
2019-04-12 22:32:43 +0200

    Added TriConsumer

---
+++ src/main/java/pw/masy/gutils/consumer/TriConsumer.java
@@ -0,0 +1,48 @@
+package pw.masy.gutils.consumer;
+
+import java.util.Objects;
+
+/**
+ * Represents and operation that accepts 3 input arguments and returns no
+ * result. This is the three-arity specialization of {@link java.util.function.Consumer}.
+ * Unlike most other functional interfaces, {@code TriConsumer} is expected
+ * to operate via side-effects.
+ *
+ * @param <T> the type if the first argument to the operation
+ * @param <U> the type of the second argument to the operation
+ * @param <V> the type of the third argument to the operation
+ */
+@FunctionalInterface
+public interface TriConsumer<T, U, V> {
+
+	/**
+	 * Performs this operation on the given arguments.
+	 *
+	 * @param t the first input argument
+	 * @param u the second input argument
+	 * @param v the third input argument
+	 */
+	void accept(T t, U u, V v);
+
+	/**
+	 * Returns a composed {@code TriConsumer} that performs, in sequence, this
+	 * operation followed by the {@code after} operation. If performing either
+	 * operation throws an exception, it is relayed to the caller of the
+	 * composed operation.  If performing this operation throws an exception,
+	 * the {@code after} operation will not be performed.
+	 *
+	 * @param after the operation to perform after this operation
+	 * @return a composed {@code TriConsumer} that performs in sequence this
+	 * operation followed by the {@code after} operation
+	 * @throws NullPointerException if {@code after} is null
+	 */
+	default TriConsumer<T, U, V> andThen(TriConsumer<? super T, ? super U, ? super V> after) {
+		Objects.requireNonNull(after);
+
+		return (a, b, c) -> {
+			accept(a, b, c);
+			after.accept(a, b, c);
+		};
+	}
+
+}
