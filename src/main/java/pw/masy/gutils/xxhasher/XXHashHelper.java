3cca5cfdecc0e76a682f588a9b661f6d813b0de1
Masy <dev@masy.pw>
2019-04-20 22:04:40 +0200

    Optimized HashHelper to support multiple hash generators

--- src/main/java/pw/masy/gutils/xxhasher/XXHashHelper.java
+++ src/main/java/pw/masy/gutils/xxhasher/XXHashHelper.java
@@ -13,49 +13,51 @@ import net.jpountz.xxhash.XXHashFactory;
 public final class XXHashHelper {
 
 	private static final XXHashFactory FACTORY = XXHashFactory.fastestInstance();
-	private static StreamingXXHash32 HASHER_32;
-	private static StreamingXXHash64 HASHER_64;
 
 	/**
-	 * Initializes the 32bit hasher with the given seed.
+	 * Creates a 32bit hasher with the given seed.
 	 *
 	 * @param seed the seed of the hasher
+	 * @return a {@link StreamingXXHash32} initialized with the given seed
 	 */
-	public static void init32(int seed) {
-		HASHER_32 = FACTORY.newStreamingHash32(seed);
+	public static StreamingXXHash32 get32BitHasher(int seed) {
+		return FACTORY.newStreamingHash32(seed);
 	}
 
 	/**
-	 * Initializes the 64bit hasher.
+	 * Creates a 64bit hasher with the given seed.
 	 *
 	 * @param seed the seed of the hasher
+	 * @return a {@link StreamingXXHash64} initialized with the given seed
 	 */
-	public static void init64(long seed) {
-		HASHER_64 = FACTORY.newStreamingHash64(seed);
+	public static StreamingXXHash64 init64(long seed) {
+		return FACTORY.newStreamingHash64(seed);
 	}
 
 	/**
 	 * Hashes the given byte array to a 32bit hash.
 	 *
+	 * @param hasher the {@link StreamingXXHash32} that will be used to generate the hash
 	 * @param data the bytes that gets hashed
 	 * @return the 32bit hash of the byte array
 	 */
-	public static int getHash32(byte[] data) {
-		HASHER_32.reset();
-		HASHER_32.update(data, 0, data.length);
-		return HASHER_32.getValue();
+	public static int getHash32(StreamingXXHash32 hasher, byte[] data) {
+		hasher.reset();
+		hasher.update(data, 0, data.length);
+		return hasher.getValue();
 	}
 
 	/**
-	 * Hashes the given byte array to a 64bit hash.
+	 * Hashes the given byte array to a 32bit hash.
 	 *
+	 * @param hasher the {@link StreamingXXHash64} that will be used to generate the hash
 	 * @param data the bytes that gets hashed
 	 * @return the 64bit hash of the byte array
 	 */
-	public static long getHash64(byte[] data) {
-		HASHER_64.reset();
-		HASHER_64.update(data, 0, data.length);
-		return HASHER_64.getValue();
+	public static long getHash64(StreamingXXHash64 hasher, byte[] data) {
+		hasher.reset();
+		hasher.update(data, 0, data.length);
+		return hasher.getValue();
 	}
 
 }
