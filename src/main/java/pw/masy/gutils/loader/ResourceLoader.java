41ee17c160df92a9ebe878654695edc9be444e5d
Marcel Schauer <dev@masy.pw>
2019-04-26 13:47:29 +0200

    Added asString methods to ResourceLoader

--- src/main/java/pw/masy/gutils/loader/ResourceLoader.java
+++ src/main/java/pw/masy/gutils/loader/ResourceLoader.java
@@ -3,6 +3,7 @@ package pw.masy.gutils.loader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.apache.commons.io.IOUtils;
@@ -38,6 +39,33 @@ public class ResourceLoader {
 	public static byte[] loadExternalResource(String path) throws IOException {
 		FileInputStream stream = new FileInputStream(path);
 		return IOUtils.toByteArray(stream);
+	}
+
+	/**
+	 * Loads an inner-jar file as a string.
+	 *
+	 * @param path the path to the file that will be loaded
+	 * @return the loaded file as string
+	 * @throws IOException when the file could not be loaded
+	 */
+	public static String loadInternalResourceAsString(String path, Charset charset) throws IOException {
+		InputStream stream = ResourceLoader.class.getResourceAsStream(path);
+		if (stream == null)
+			throw new IOException("Could not load resource \"" + path + "\". File not found.");
+
+		return IOUtils.toString(stream, charset);
+	}
+
+	/**
+	 * Loads an outer-jar file as a string.
+	 *
+	 * @param path the path to the file that will be loaded
+	 * @return the loaded file as string
+	 * @throws IOException when the file could not be loaded
+	 */
+	public static String loadExternalResourceAsString(String path, Charset charset) throws IOException {
+		FileInputStream stream = new FileInputStream(path);
+		return IOUtils.toString(stream, charset);
 	}
 
 }
